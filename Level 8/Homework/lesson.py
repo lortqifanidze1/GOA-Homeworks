1ab158e0fc91d07e34bfadc599e3340828dcf365
lortkifandize1 <lortkifanidzeluka504@gmail.com>
2024-06-06 19:31:05 +0400

    commit

---
+++ Level 8/Homework/lesson.py
@@ -0,0 +1,35 @@
+num = 5 #aq cven vigeb cvlad romelic = 5-ts
+
+
+
+
+print(num >= 1 and num <= 10) # aq cven num anu 5 vaketebs magalit, 5 metia an tolia 1  ze da 5 naklebia an tolia atze amitom rogor vicit true and true = true
+print(num >= 1 and num <= 4) # aqac igive magram boloshi 5 > da arc tolia 4 ze amitom ,rogorc vicit true and fase aris false
+print(num > 5 and num <= 10) #  aq  5 araaa 5ze mati amitom false aris da meoresi 5 naklebia 10 amitom (fasle da true) = false
+print(num > 5 and num > 10) # 
+
+
+
+print(num >= 1 or num <= 10) # aq qva or rac and is sapirispiroa aq 5 metia 1 an 5 , xutse  amitom es sworea anu true  
+print(num >= 1 or num <= 4) # aqac dasasyishi true gvaq margam an anu or shi araswore imitom ro 5 metia 4 ze  margam rogorc vicit true or falsi true igebs  imitom rom erti sworea 
+print(num > 5 or num <= 10) # aqac arasworea dasawyisi magram sworea meore magaliti imitom ro 5 , 10 da erti tru rom gvaq anu es mtlianad true gamodis 
+print(num > 5 or num > 10) #  magamr roca oriveshi false aris mashin es false aris 
+
+
+num1 = 1 
+print("-------------------------------And------------------------------------")
+print (num1 >= 2 and num1 == 1  ) #false
+print (num1 <= 2 and num1 > 0) #true 
+print(num1 < 1 and num1 == 2 ) #false 
+print(num1 == 1 and num1 > 0) #true
+print(num1 + num1 > num1 and  num1 - num1 == 0)
+
+
+print("-------------------------Or-----------------------------------------")
+num2 = 2
+
+print(num2 > 1 or num2 <= 1 )
+print(num2  > 2  or num2 < 1)
+print(num2 - 1 + 2 > 3 or num2 > 3)
+print(num2 >= 2 and num2 < 1)
+print( num2 + num2 > 4 or num2 - 1 > 3)
