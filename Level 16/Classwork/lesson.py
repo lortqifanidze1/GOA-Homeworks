7020670bc5c1ce8ef6723f83d0f8180261affef7
lortkifandize1 <lortkifanidzeluka504@gmail.com>
2024-06-18 17:34:57 +0400

    commit

---
+++ Level 16/Classwork/lesson.py
@@ -0,0 +1,7 @@
+age = int(input("PLease enrter you age"))
+if age >= 5 and age <13:
+    print("your kid ")
+elif age >=13 and age  <18:
+    print("your teen")
+else:
+    print("old enogth")
---
+++ Level 16/Classwork/lesson1.py
@@ -0,0 +1,8 @@
+films = ["avergens", "spider mon" "iron man " , "batman"] #varchevt sayvarel filmess [] ebshi
+music = ["ar myavs", "sayvareli"  "musikebi"] #varchevs sayvarel musikisa da vwert [] shi
+ertad = films + music #vaertianeb orive sias 
+print(ertad) # vprintavt gaertianebul sias
+print(len(films)) #da len daxmarebit vedzebs ramdeni monacemina
+print(len(music))
+print(films[0])
+print(music[0])
