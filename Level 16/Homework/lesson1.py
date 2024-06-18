c3aee6de92c7ff222b11576b9b39647827367dba
lortkifandize1 <lortkifanidzeluka504@gmail.com>
2024-06-19 00:04:59 +0400

    commit

---
+++ Level 16/Homework/lesson1.py
@@ -0,0 +1,18 @@
+saxeli =["nino","goga"]
+num = [1, 2, 3, 4, 5,6 ,7,8,9,10]
+luwi_num = [10,11,12,13,14,15,16]
+luwi_num [0] = 1
+luwi_num [2] = 1
+luwi_num [4] = 1
+luwi_num [6] = 1
+per = [input("please enter your :email , name ,lastname , home addres : ")]
+gvari = ["l","o","r","t","q","i","f","a","n","i","d","z","e"]
+
+
+print(saxeli[0])
+print(saxeli[1])
+print(num[0])
+print(num[9])
+print(luwi_num)
+print(per)
+print(gvari)
