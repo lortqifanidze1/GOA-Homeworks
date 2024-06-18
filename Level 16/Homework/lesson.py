63bdce3c25510323a5014abb200d58d69a0b8300
lortkifandize1 <lortkifanidzeluka504@gmail.com>
2024-06-18 17:43:29 +0400

    commit

---
+++ Level 16/Homework/lesson.py
@@ -0,0 +1,17 @@
+cxrili1 = ["ferrari", "bugati", "porche" , "bmw"]
+cxrili2 = ["vashli","atami", "banani" "alobuli"]
+cxrili3 = ["avergens", "hulk", "spider man", "dr.strage"] 
+cxrili4 = ["shua qalaqi", "prime house" ,"chemi colis dakalebi"]
+cxrili5 = cxrili1 + cxrili2 + cxrili3 + cxrili4
+
+print(cxrili5)
+print(len(cxrili1))
+print(len(cxrili2))
+print(len(cxrili3))
+print(len(cxrili4))
+print(len(cxrili5))
+print(cxrili1 [0])
+print(cxrili2 [1])
+print(cxrili3 [2])
+print(cxrili4 [1])
+print(cxrili5 [3])
