8c6deb0ba1e19c6a51d4fa7c8b4e51a5fd15bf2e
lortkifandize1 <lortkifanidzeluka504@gmail.com>
2024-07-22 18:48:23 +0400

    commit

---
+++ Level 30/Classwork/lesson1.py
@@ -0,0 +1,12 @@
+def filter_odd(numbers):
+    even_numbers = []
+    for num in numbers:
+        if num % 2 == 0:
+           pass
+        else:
+           return even_numbers
+        continue 
+        even_numbers.append(num)
+        
+result = filter_odd([1,2,3,4,5,6,7,8,9])
+print(result)
