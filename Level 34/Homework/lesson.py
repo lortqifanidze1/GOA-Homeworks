5c1d510baf794314e5c28af5f9dc799e29982528
lortqifanidze1 <lortkifanidzeluka504@gmail.com>
2024-07-28 18:45:36 +0400

    commit

---
+++ Level 34/Homework/lesson.py
@@ -0,0 +1,36 @@
+# def find_short(s):# valetebt funcias saxelasd find shots da vadzevs vclad s 
+#     list1 = s.split(" ") # aq list1 vatolebs s anucu vcladi.split daavlie gamotoveba 
+
+
+#     word_len = len(list1[0])#vigebs kide ert cvlad da vatolebt lenlists 1 elements 
+#     for i in list1: #for loopit vuvlit am cvlads lits 1 
+#         if len(i) < word_len: #dav vwert tu lev(i) for loopis cvladi < wold lenze mashin world len gautote len(I) 
+
+#             word_len = len(i)
+    
+#     # word_len = 3
+#     return word_len #da vabrunebt words 
+
+# print(find_short("bitcoin take over the world maybe who knows perhaps"))
+
+
+
+txt = "aloqcomosta"
+x = txt.split("q")
+print(x)
+txt = "aloqcomostaqamigo"
+x = txt.split("q")
+print(x)
+txt = "aloqcomosta"
+x = txt.split("a")
+print(x)
+txt = "aloqcomosta"
+x = txt.split("lo")
+print(x)
+txt = "aloqcomosta"
+x = txt.split("q")
+print(x)
+txt = "alocomosta"
+x = txt.split("comosta")
+print(x)
+
