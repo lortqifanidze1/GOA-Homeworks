c1acb2e05d420ecc2ad5eb2483ed867f36355a4b
lortqifanidze1 <lortkifanidzeluka504@gmail.com>
2024-08-22 12:12:42 +0400

    commit

---
+++ Level 48/Homework/codewars.py
@@ -0,0 +1,9 @@
+# a = ["1 https://www.codewars.com/kata/558fc85d8fd1938afb000014
+
+# # 2 https://www.codewars.com/kata/5aba780a6a176b029800041c
+
+# # 3 https://www.codewars.com/kata/514a6336889283a3d2000001
+
+# # 4 https://www.codewars.com/kata/5a3dd29055519e23ec000074
+
+# # 5 https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9/train/python"]
